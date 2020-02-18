34c4c4031201fe8c23f0d5c6579f17be5c6d65be
nghiatc <congnghia0609@gmail.com>
2020-02-18 13:38:33 +0700

    update code

--- main.rb
+++ main.rb
@@ -107,3 +107,59 @@ status == :pending #=> true
 status == 'pending' #=> false
 status == :approved #=> false
 
+
+# Arrays
+# This is an array
+array = [1, 2, 3, 4, 5]
+#puts array
+#=> [1, 2, 3, 4, 5]
+
+# Arrays can contain different types of items
+items = [1, "hello", false]
+#puts items
+#=> [1, "hello", false]
+
+# Arrays can be indexed
+# From the front
+array[0] #=> 1
+array[12] #=> nil
+
+# Like arithmetic, [var] access is just syntactic sugar
+# for calling a method [] on an object
+array.[] 0 #=> 1
+array.[] 12 #=> nil
+
+# From the end
+array[-1] #=> 5
+
+# With a start index and length
+array[2, 3] #=> [3, 4, 5]
+
+# Or with a range
+array[1..3] #=> [2, 3, 4]
+
+# Add to an array like this
+array << 6 #=> [1, 2, 3, 4, 5, 6]
+
+
+# Hashes are Ruby's primary dictionary with keys/value pairs.
+# Hashes are denoted with curly braces:
+hash = {'color' => 'green', 'number' => 5}
+hash.keys #=> ['color', 'number']
+# puts hash.to_s
+#=> {"color"=>"green", "number"=>5}
+# Hashes can be quickly looked up by key:
+hash['color'] #=> 'green'
+hash['number'] #=> 5
+# Asking a hash for a key that doesn't exist returns nil:
+hash['nothing here'] #=> nil
+
+# Since Ruby 1.9, there's a special syntax when using symbols as keys:
+new_hash = { defcon: 3, action: true}
+new_hash.keys #=> [:defcon, :action]
+
+# Tip: Both Arrays and Hashes use Enumerable
+# They share a lot of useful methods such as: each, map, count, and more...
+
+
+
