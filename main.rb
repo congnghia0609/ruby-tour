967dd84f7119c6bbd629fe62e0c07853f3c5820b
nghiatc <congnghia0609@gmail.com>
2020-02-18 13:33:41 +0700

    first commit

---
+++ main.rb
@@ -0,0 +1,26 @@
+## Init project
+# mkdir ruby-tour
+# cd ruby-tour
+# touch main.rb
+
+## Run
+# ruby main.rb
+
+# https://www.vikingcodeschool.com/professional-development-with-ruby/ruby-cheat-sheet
+
+
+# This is a comment
+=begin
+This is a multiline comment
+No-one uses them
+You shouldn't either
+=end
+
+# print to the output
+puts "print string"
+
+# First and foremost: Everything is an object.
+
+# Numbers are objects
+3.class #=> Fixnum
+3.to_s #=> "3"
