fa2d59811b45dec5d380d5bdfe5c03b3cee444cb
nghiatc <congnghia0609@gmail.com>
2020-02-18 13:35:06 +0700

    update code

--- main.rb
+++ main.rb
@@ -24,3 +24,45 @@ puts "print string"
 # Numbers are objects
 3.class #=> Fixnum
 3.to_s #=> "3"
+
+# Some basic arithmetic
+1 + 1 #=> 2
+8 - 1 #=> 7
+10 * 2 #=> 20
+35 / 5 #=> 7
+2 ** 5 #=> 32
+
+# Arithmetic is just syntactic sugar for calling a method on an object
+1.+(3) #=> 4
+10.* 5 #=> 50
+
+# Special values are objects
+nil # Nothing to see here
+true # truth
+false # falsehood
+
+nil.class #=> NilClass
+true.class #=> TrueClass
+false.class #=> FalseClass
+
+# Equality
+1 == 1 #=> true
+2 == 1 #=> false
+
+# Inequality
+1 != 1 #=> false
+2 != 1 #=> true
+!true  #=> false
+!false #=> true
+
+# apart from false itself, nil is the only other 'falsy' value
+!nil   #=> true
+!false #=> true
+!0     #=> false
+
+# More comparisons
+1 < 10 #=> true
+1 > 10 #=> false
+2 <= 2 #=> true
+2 >= 2 #=> true
+
